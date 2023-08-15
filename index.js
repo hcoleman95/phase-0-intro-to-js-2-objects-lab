935bb6e775677a0de80658a74a8de81b785c00ec
Howard Coleman <howard.coleman16@gmail.com>
2023-08-15 10:39:06 -0400

    complete objects

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Howard",
+    streetAddress: "604 s 60th st",
+};
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value ){
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+
+    return newEmployee;  
+   }
+   
+   function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+        obj[key] =value;
+        
+        return obj;
+   }
+    
+
+    function deleteFromEmployeeByKey(obj, key){
+        const newObj = {...obj};
+        delete newObj[key];
+        
+        return newObj;
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(obj, key){
+        delete obj[key];
+        
+        return obj;
+    }
